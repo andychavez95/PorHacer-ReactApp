cbab7a52f15b7b86cb3c8e43e82333d7e06c27c5
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-18 17:37:40 -0500

    Refactorización de la UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,10 @@ const App = () => {
                         <h1 className="uppercase text-white text-3xl tracking-widest font-bold">Por hacer</h1>
                         <button><Moon fill="#fff"/></button>
                     </div>
-                    <form className="mt-8">
+                </header>
+                <main className="container mx-auto px-4 mt-8">
+                    { /* Registro de tareas. */ }
+                    <form className>
                         <div className="flex items-center gap-4 overflow-hidden rounded-md bg-white py-4 px-4">
                             <span className="inline-block h-5 w-5 rounded-full border-2"></span>
                             <input 
@@ -21,9 +24,18 @@ const App = () => {
                             />
                         </div>
                     </form>
-                </header>
-                <main className="container mx-auto px-4 mt-8">
-                    <div className="rounded-md bg-white">
+                    {/* Lista de tareas. */}
+                    <div className="rounded-md bg-white mt-8">
+                        <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
+                            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                            <p className="grow text-gray-600">aqui va una tarea</p>
+                            <button className="flex-none"><Cross/></button>
+                        </article>
+                        <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
+                            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
+                            <p className="grow text-gray-600">aqui va una tarea</p>
+                            <button className="flex-none"><Cross/></button>
+                        </article>
                         <article className="flex gap-4 border-b border-b-gray-400 py-4 px-4">
                             <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
                             <p className="grow text-gray-600">aqui va una tarea</p>
@@ -44,17 +56,18 @@ const App = () => {
                             <button className="text-gray-600">quedan x tareas</button>
                         </section>
                     </div>
+                    {/* Operaciones con la lista de tareas. */}
+                    <section className="container mx-auto mt-8">
+                        <div className="flex justify-center gap-8 bg-white rounded-md p-4">
+                            <button className="hover:text-blue-600 font-bold">Todos</button>
+                            <button className="hover:text-blue-600 font-bold">Activas</button>
+                            <button className="hover:text-blue-600 font-bold">Completadas</button>
+                        </div>
+                    </section>
                 </main>
-                <section className="container mx-auto px-4 mt-8">
-                    <div className="flex justify-center gap-4 bg-white rounded-md p-4">
-                        <button className="hover:text-blue-600">Todos</button>
-                        <button className="hover:text-blue-600">Activas</button>
-                        <button className="hover:text-blue-600">Completadas</button>
-                    </div>
-                </section>
-                <section className="container mx-auto px-4 mt-8">
+                <footer className="container mx-auto px-4 mt-8">
                     <p className="text-center text-gray-600">Arrastra y suelta para reordenar la lista</p>
-                </section>
+                </footer>
             </div>
         </Fragment>
     );
