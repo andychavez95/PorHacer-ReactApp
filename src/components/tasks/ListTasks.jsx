770600cf455a00af1d96889faa81ddd9047a51a3
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-18 18:54:01 -0500

    Elementos de separados de la lista

--- src/components/tasks/ListTasks.jsx
+++ src/components/tasks/ListTasks.jsx
@@ -1,33 +1,19 @@
-import Cross from "../icons/Cross"
+import ItemTask from "./ItemTask";
 
 const ListTasks = () => {
     return (
         <div className="rounded-md bg-white mt-8">
-            <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
-                <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-                <p className="grow text-gray-600">aqui va una tarea</p>
-                <button className="flex-none"><Cross/></button>
-            </article>
-            <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
-                <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-                <p className="grow text-gray-600">aqui va una tarea</p>
-                <button className="flex-none"><Cross/></button>
-            </article>
-            <article className="flex gap-4 border-b border-b-gray-400 py-4 px-4">
-                <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-                <p className="grow text-gray-600">aqui va una tarea</p>
-                <button className="flex-none"><Cross/></button>
-            </article>
-            <article className="flex gap-4 border-b border-b-gray-400 py-4 px-4">
-                <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-                <p className="grow text-gray-600">aqui va una tarea</p>
-                <button className="flex-none"><Cross/></button>
-            </article>
-            <article className="flex gap-4 border-b border-b-gray-400 py-4 px-4">
-                <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-                <p className="grow text-gray-600">aqui va una tarea</p>
-                <button className="flex-none"><Cross/></button>
-            </article>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
+            <ItemTask/>
         </div>
     );
 }
