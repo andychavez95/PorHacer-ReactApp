a1bf0346b09fe578ff7daa2cc92a6d48bfcfa5a2
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-19 11:23:18 -0500

    Agregando tareas de ejemplo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Header";
 import FormTask from "./components/tasks/FormTask";
 import ListTasks from "./components/tasks/ListTasks";
 import CounterTasks from "./components/tasks/CounterTasks";
 import Footer from "./components/Footer";
 
+const initialTask = [
+    {
+        id: 1,
+        message: "Primer tarea de ejemplo",
+        completed: false
+    },
+    {
+        id: 2,
+        message: "Segunda tarea de ejemplo",
+        completed: true
+    },
+    {
+        id: 3,
+        message: "Tercer tarea de ejemplo",
+        completed: false
+    }
+];
+
 const App = () => {
+
+    const [tasks, setTasks] = useState(initialTask);
+
+    const deleteTask = (idTask) => {
+        const newArrayTaks = tasks.filter((task) => task.id !== idTask);
+        setTasks([ ...newArrayTaks ]);
+    }
+
     return (
         <Fragment>
             <div className="bg-[url('./assets/bg-mobile-light.jpg')] bg-contain bg-no-repeat bg-gray-200 min-h-screen">
@@ -14,7 +40,7 @@ const App = () => {
                     { /* Registro de tareas. */ }
                     <FormTask />
                     {/* Lista de tareas. */}
-                    <ListTasks />
+                    <ListTasks tasks={ tasks } deleteTask={ deleteTask } />
                     {/* Contador de tareas */}
                     <CounterTasks />
                     {/* Operaciones con la lista de tareas. */}
