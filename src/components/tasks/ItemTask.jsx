d7a311f419486f7b38ba1696bbbbb0be7a19d8e6
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-19 11:21:43 -0500

    Función para borrar una tarea

--- src/components/tasks/ItemTask.jsx
+++ src/components/tasks/ItemTask.jsx
@@ -1,11 +1,21 @@
 import Cross from "../icons/Cross"
 
-const ItemTask = () => {
+/*
+ * Muestra una tarea.
+ * Desde este componente puedes borrar la tarea o cambiar su estado
+ * a completado.
+ */
+const ItemTask = ({ task, deleteTask }) => {
+    
+    const handleOnClick = (idTask) => {
+        deleteTask(idTask);
+    }
+
     return (
         <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
             <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-            <p className="grow text-gray-600">aqui va una tarea</p>
-            <button className="flex-none"><Cross/></button>
+            <p className="grow text-gray-600">{ task.message }</p>
+            <button onClick={ () => handleOnClick(task.id) } className="flex-none"><Cross/></button>
         </article>
     );
 }
