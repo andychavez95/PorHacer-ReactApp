66ce40d3f9b4d84fb0b00d15574d257f8c837241
Hristo Chavez <hristoandychavezhuertas@gmail.com>
2024-03-19 13:59:12 -0500

    Función para marcar como completa una tarea

--- src/components/tasks/ItemTask.jsx
+++ src/components/tasks/ItemTask.jsx
@@ -5,17 +5,34 @@ import Cross from "../icons/Cross"
  * Desde este componente puedes borrar la tarea o cambiar su estado
  * a completado.
  */
-const ItemTask = ({ task, deleteTask }) => {
+const ItemTask = ({ task, deleteTask, completeTask }) => {
     
     const handleOnClick = (idTask) => {
         deleteTask(idTask);
     }
 
+    const onClickCompleteTask = (idTask) => {
+        completeTask(idTask);
+    } 
+
     return (
         <article className="flex items-center gap-4 border-b border-b-gray-400 py-4 px-4">
-            <button className="inline-block h-5 w-5 flex-none rounded-full border-2"></button>
-            <p className="grow text-gray-600">{ task.message }</p>
-            <button onClick={ () => handleOnClick(task.id) } className="flex-none"><Cross/></button>
+            <button 
+                onClick={ () => onClickCompleteTask(task.id) } 
+                className="inline-block h-5 w-5 flex-none rounded-full border-2"
+            >
+            </button>
+            <p 
+                className={`grow text-gray-600 ${task.completed && "line-through"}`}
+            >
+                { task.message }
+            </p>
+            <button 
+                onClick={ () => handleOnClick(task.id) } 
+                className="flex-none"
+            >
+                <Cross/>
+            </button>
         </article>
     );
 }
--- src/components/tasks/ListTasks.jsx
+++ src/components/tasks/ListTasks.jsx
@@ -3,12 +3,17 @@ import ItemTask from "./ItemTask";
 /*
  * Muestra la lista de tareas.
  */
-const ListTasks = ({ tasks, deleteTask }) => {
+const ListTasks = ({ tasks, deleteTask, completeTask }) => {
     return (
         <div className="rounded-md bg-white mt-8">
             {
                 tasks.map(task => (
-                    <ItemTask task={ task } key={ task.id } deleteTask={ deleteTask }/>
+                    <ItemTask 
+                        task={ task } 
+                        key={ task.id } 
+                        deleteTask={ deleteTask } 
+                        completeTask={ completeTask }
+                    />
                 ))
             }
         </div>
